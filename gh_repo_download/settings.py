aa94f0a8be29f265f9ec501e3d7387ce88d94aa8
Dustin Wyatt <dustin.wyatt@gmail.com>
2024-03-19 14:59:01 -0500

    Update ALLOWED_HOSTS setting in gh_repo_download
    Changed the ALLOWED_HOSTS in settings from an empty list to be dynamically obtained based on the environment variables. This allows the hosts to be set dynamically based on the running environment.

--- gh_repo_download/settings.py
+++ gh_repo_download/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 
 # Application definition
