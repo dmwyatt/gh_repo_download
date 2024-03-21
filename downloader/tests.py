77567220071c0adb122682a7450432c72956d367
Dustin Wyatt <dustin.wyatt@gmail.com>
2024-03-21 16:28:53 -0500

    Add tests for RepositoryForm validation
    These new tests check the behavior of the RepositoryForm. They ensure the form correctly validates user inputs, handling both valid and invalid cases. This includes tests for a form with valid input, invalid input, no input, and multiple valid inputs.

--- downloader/tests.py
+++ downloader/tests.py
@@ -1,6 +1,6 @@
 import pytest
 from django.core.exceptions import ValidationError
-from .forms import validate_repo_url
+from .forms import RepositoryForm, validate_repo_url
 
 
 # A happy path test
@@ -40,3 +40,62 @@ def test_validate_repo_url_edgecases(url, exception_msg):
     with pytest.raises(ValidationError) as exc:
         validate_repo_url(url)
     assert str(exc.value.messages[0]) == exception_msg
+
+
+def test_form_with_valid_input():
+    # Arrange
+    form = RepositoryForm({"repo_url": "https://github.com/username/repo_name"})
+    expected_username = "username"
+    expected_repo_name = "repo_name"
+
+    # Act
+    is_valid = form.is_valid()
+
+    # Assert
+    assert is_valid is True, f"Error in form validation: {form.errors}"
+    assert form.cleaned_data["username"] == expected_username
+    assert form.cleaned_data["repo_name"] == expected_repo_name
+
+
+def test_form_with_invalid_input():
+    # Arrange
+    form = RepositoryForm({"repo_url": "Not a valid URL"})
+
+    # Act
+    is_valid = form.is_valid()
+
+    # Assert
+    assert is_valid is False
+    assert "repo_url" in form.errors
+
+
+def test_form_without_input():
+    # Arrange
+    form = RepositoryForm({})
+
+    # Act
+    is_valid = form.is_valid()
+
+    # Assert
+    assert is_valid is False
+    assert "repo_url" in form.errors
+
+
+@pytest.mark.parametrize(
+    "repo_url,username,repo_name",
+    [
+        ("https://github.com/username1/repo1", "username1", "repo1"),
+        ("https://github.com/username2/repo2", "username2", "repo2"),
+    ],
+)
+def test_form_with_multiple_valid_inputs(repo_url, username, repo_name):
+    # Arrange
+    form = RepositoryForm({"repo_url": repo_url})
+
+    # Act
+    is_valid = form.is_valid()
+
+    # Assert
+    assert is_valid is True, f"Error in form validation: {form.errors}"
+    assert form.cleaned_data["username"] == username
+    assert form.cleaned_data["repo_name"] == repo_name
