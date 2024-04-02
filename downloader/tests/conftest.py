51371ec418ab4cf8e2def265d0a85f3bc907e5f5
Dustin Wyatt <dustin.wyatt@gmail.com>
2024-04-02 15:49:09 -0500

    Add timeout settings to pytest configuration
    A timeout configuration has been added to the pytest settings in conftest.py. This update includes the addition of a fixture to set the default timeout. Moreover, the expect options have been adjusted to include a timeout setting.

--- downloader/tests/conftest.py
+++ downloader/tests/conftest.py
@@ -1,9 +1,23 @@
 import os
+
 import nest_asyncio
+import pytest
 
 os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
+
+from playwright.sync_api import expect
+
+timeout = 6_000
+
+expect.set_options(timeout=timeout)
 
 
 def pytest_configure():
     # https://github.com/microsoft/playwright-pytest/issues/167#issuecomment-1546854047
     nest_asyncio.apply()
+
+
+@pytest.fixture
+def context(context):
+    context.set_default_timeout(timeout)
+    yield context
