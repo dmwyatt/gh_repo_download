82b6bd979516dac5aa1d178fc35980d8747e2b84
Dustin Wyatt <dustin.wyatt@gmail.com>
2024-06-28 09:20:07 -0500

    Add tree selection validator

--- static/js/tree.js
+++ static/js/tree.js
@@ -24,16 +24,25 @@ class Tree {
 }
 
 class TreeStateManager {
-  constructor() {
+  constructor(selectionValidator = () => true) {
     this.state = {
       selectedItems: new Map(), // Node -> boolean or 'indeterminate'
     };
+    this.selectionValidator = selectionValidator;
   }
-
   toggleSelection(node) {
     const currentState = this.state.selectedItems.get(node) || false;
     const newState = !currentState;
-    this.updateSelection(node, newState);
+
+    // Check if the new selection state is valid
+    if (this.selectionValidator(this.getSelectedNodes(), node, newState)) {
+      this.updateSelection(node, newState);
+      this.emitSelectionChangedEvent();
+      return true; // Selection was successful
+    } else {
+      this.emitSelectionInvalidEvent(node, newState);
+      return false; // Selection was prevented
+    }
   }
 
   updateSelection(node, isSelected) {
@@ -49,6 +58,26 @@ class TreeStateManager {
     if (node.parent) {
       this.updateParentSelection(node.parent);
     }
+  }
+
+  getSelectedNodes() {
+    return Array.from(this.state.selectedItems.entries())
+      .filter(([node, isSelected]) => isSelected === true)
+      .map(([node]) => node);
+  }
+
+  emitSelectionChangedEvent() {
+    const event = new CustomEvent("selectionChanged", {
+      detail: { selectedNodes: this.getSelectedNodes() },
+    });
+    document.dispatchEvent(event);
+  }
+
+  emitSelectionInvalidEvent(node, attemptedState) {
+    const event = new CustomEvent("selectionInvalid", {
+      detail: { node, attemptedState },
+    });
+    document.dispatchEvent(event);
   }
 
   updateParentSelection(node) {
